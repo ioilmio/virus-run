3597b0f19ff390bc694fc69e2037a0601659976e
ioilmio <illuminato.salvatore@gmail.com>
2020-12-16 01:18:12 +0100

    linters;webpack config;

---
+++ dist/main.js
@@ -0,0 +1,1 @@
+(()=>{const o=document.createElement("h1");document.body.appendChild(o),o.textContent="Hello",console.log("hello"),console.log("hello"),console.log("hello")})();
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/index.js',
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'webpack',
+      templates: 'src/index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+      {
+        test: /\.html$/i,
+        use: [
+          'html-loader',
+        ],
+      },
+    ],
+  },
+};
